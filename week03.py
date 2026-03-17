3f15f6627725453d40ec4e80c119493e18177bfe
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-17 17:16:33 +0900

    week03 numpy ii

--- week03.py
+++ week03.py
@@ -1,25 +1,16 @@
 import numpy as np
 
-# data1 = np.array([40,30,20,10])
-# data1 = np.array([40,30.0,20,10])
-# data1 = np.array([40,30,20,10], dtype = float)
-data1 = np.array([40,30.0,"inha",10])
-print(data1)
+# items = [40,7,99,-3]
+# items_new = []
+# for item in items:
+#     items_new.append(item+5)
+# print(items_new)
 
-data2 = np.array([[1,2] , [3,4]])
-print(data2)
+items = [40,7,99,-3]
+items = list(map(lambda i: i+5, items))
+print(items)
 
-data3 = np.zeros((3,4,2))
-print(data3)
-
-data4 = np.ones((8))
-print(data4)
-# arange, full, linspace, np.random.rand()
-
-print(data1.dtype, data2.dtype, data3.dtype, data4.dtype)
-print(data1.ndim, data2.ndim, data3.ndim, data4.ndim)
-print(data1.shape, data2.shape, data3.shape, data4.shape)
-# size, T, itemsize ...
-
-
-
+data1 = np.array([40,7,99,-3])
+print(data1 + 5)
+print(data1 * 3)
+print(data1 > 14)
