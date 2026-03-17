320f46fa3995ea01b370d426401d7e29a1179abe
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-17 17:51:23 +0900

    week03 dataframe i

--- week03.py
+++ week03.py
@@ -1,11 +1,10 @@
-import numpy as np
+import pandas as pd
 
-scores = np.array([
-    #국, 영, 수
-    [80,100,90], # A 학생
-    [85,76,95], # B 학생
-    [89,85,87] # C 학생
-])
-print(f"전체 평균 : {np.mean(scores)}")
-print(f"국 영 수 과목별 평균 : {np.mean(scores, axis=0)}") # 열 기준 연산
-print(f"A학생, B학생, C학생 최고 점수 : {np.max(scores, axis=1)}")
+items = {
+    'a': [100,80,90],
+    'b': [95,75,85],
+    'c': [70,100,99]
+}
+
+df_items = pd.DataFrame(items, index=[1,2,3])
+print(df_items)
