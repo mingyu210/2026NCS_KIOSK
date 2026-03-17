ab006624b9b7744166c3ee92fab496f181798926
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-17 18:48:27 +0900

    week03 dataframe iv

--- week03.py
+++ week03.py
@@ -1,13 +1,23 @@
 import pandas as pd
+import seaborn as sns
 
-items = [
-    [100,95,70],
-    [80,75,100],
-    [90,85,99]
-]
+mpg = sns.load_dataset("mpg")
+# print(mpg.head(3))
+# print(mpg.tail(3))
+# print(mpg.query('mpg > 27.0'))
+mpg_asc = mpg.sort_values("mpg", ascending=True)
+print(mpg_asc)
+mpg_asc_disp_out = mpg_asc.drop(columns="displacement")
+print(mpg_asc_disp_out)
 
-df_items = pd.DataFrame(items, index=[1,2,3], columns=['a', 'b', 'c'])
-print(df_items)
-df_items_melt = pd.melt(df_items).rename(columns={'variable': 'var', 'value': 'val'}).query('val >= 85')
-print(df_items_melt)
+# items = [
+#     [100,95,70],
+#     [80,75,100],
+#     [90,85,99]
+# ]
+#
+# df_items = pd.DataFrame(items, index=[1,2,3], columns=['a', 'b', 'c'])
+# print(df_items)
+# df_items_melt = pd.melt(df_items).rename(columns={'variable': 'var', 'value': 'val'}).query('val >= 85')
+# print(df_items_melt)
 
