896a3ff41c1b21b4fa6534b746d409a0db2c1f80
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-17 17:54:04 +0900

    week03 dataframe ii

--- week03.py
+++ week03.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-items = {
-    'a': [100,80,90],
-    'b': [95,75,85],
-    'c': [70,100,99]
-}
+items = [
+    [100,95,70],
+    [80,75,100],
+    [90,85,99]
+]
 
-df_items = pd.DataFrame(items, index=[1,2,3])
+df_items = pd.DataFrame(items, index=[1,2,3], columns=['a', 'b', 'c'])
 print(df_items)
