e5c6be9be2c8e9f496acded77ed96bdb7926a583
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-17 17:07:19 +0900

    week03 numpy i

--- week03.py
+++ week03.py
@@ -1,4 +1,25 @@
-import pandas as pd
-data = [10, 20, 30, 40]
-average = pd.Series(data).mean()
-print(average)
+import numpy as np
+
+# data1 = np.array([40,30,20,10])
+# data1 = np.array([40,30.0,20,10])
+# data1 = np.array([40,30,20,10], dtype = float)
+data1 = np.array([40,30.0,"inha",10])
+print(data1)
+
+data2 = np.array([[1,2] , [3,4]])
+print(data2)
+
+data3 = np.zeros((3,4,2))
+print(data3)
+
+data4 = np.ones((8))
+print(data4)
+# arange, full, linspace, np.random.rand()
+
+print(data1.dtype, data2.dtype, data3.dtype, data4.dtype)
+print(data1.ndim, data2.ndim, data3.ndim, data4.ndim)
+print(data1.shape, data2.shape, data3.shape, data4.shape)
+# size, T, itemsize ...
+
+
+
