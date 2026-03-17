6ccf646c6b0870530634f95dd1735f4cd2f9730e
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-17 17:38:35 +0900

    week03 numpy iv

--- week03.py
+++ week03.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-items = [40,7,99,-3]
-print(np.sum(items))
-print(np.mean(items))
-print(np.max(items))
-print(np.min(items))
-print(np.median(items))
-print(np.var(items))
-print(np.std(items))
+scores = np.array([
+    #국, 영, 수
+    [80,100,90], # A 학생
+    [85,76,95], # B 학생
+    [89,85,87] # C 학생
+])
+print(f"전체 평균 : {np.mean(scores)}")
+print(f"국 영 수 과목별 평균 : {np.mean(scores, axis=0)}") # 열 기준 연산
+print(f"A학생, B학생, C학생 최고 점수 : {np.max(scores, axis=1)}")
