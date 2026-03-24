93e54d306b815d10d4f0f1a74b583eb4d159963f
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-24 18:03:48 +0900

    week04 dataframe xii

--- week04.py
+++ week04.py
@@ -3,7 +3,7 @@ import seaborn as sns
 
 mpg = sns.load_dataset("mpg")
 print(mpg.info())
-# inplace= True  -> 원본을 바꿔라
-# mpg.dropna(subset=['horsepower'], inplace=True) 원본 업데이트
-mpg_lcean = mpg.dropna(subset=['horsepower']) # 사본 생성
-print(mpg_lcean.info())
+hp_median = mpg['horsepower'].median()
+mpg_filled = mpg.copy()
+mpg_filled['horsepower'] = mpg_filled['horsepower'].fillna(hp_median)
+print(mpg_filled.info())
