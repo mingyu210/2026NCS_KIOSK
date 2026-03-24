e794391eddced1274d2160a02171546bee95ab02
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-24 17:05:11 +0900

    week04 dataframe vii

--- week04.py
+++ week04.py
@@ -1,11 +1,13 @@
 import seaborn as sns
 # 유럽 제조 차량 중 연비가 30.0 mpg 이상인 행을 추출하시오.
 # 단, 자동차 model명, 연비, 제조국가만 표시하고 연비가 높은 순으로 정렬하시오.
+# 위 조건과 같이 원본 데이터를 업데이트하고 나머지 칼럼들은 삭제하시오.
 
 mpg = sns.load_dataset("mpg")
-print(
-    mpg[['origin', 'mpg', 'name']]
-    .query('origin == "europe" and mpg >= 30.0')
-    .sort_values(by='mpg', ascending=False)
-)
-
+print(mpg.info())
+mpg = (mpg.drop(columns=['cylinders', 'horsepower', 'displacement', 'horsepower', 'weight', 'acceleration','model_year'])
+               .query('origin == "europe" and mpg >= 30.0')
+               .sort_values(by='mpg', ascending=False)
+               )
+print(mpg)
+print(mpg.info())
