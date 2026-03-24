a65c31d005ad5b44e4fbabf427fcffab23816ce0
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-03-24 16:53:32 +0900

    week04 dataframe V

--- week03.py
+++ week03.py
@@ -1,11 +1,21 @@
+import pandas as pd
 import seaborn as sns
-# 유럽 제조 차량 중 연비가 30.0 mpg 이상인 행을 추출하시오.
-# 단, 자동차 model명, 연비, 제조국가만 표시하고 연비가 높은 순으로 정렬하시오.
 
 mpg = sns.load_dataset("mpg")
-print(
-    mpg[['origin', 'mpg', 'name']]
-    .query('origin == "europe" and mpg >= 30.0')
-    .sort_values(by='mpg', ascending=False)
-)
+print(mpg.head(3))
+print(mpg.iat[1,8])
+# print(mpg.iloc[:, [0, 1]])
+# print(mpg.iloc[2:5, [0,1]])
+# print(mpg.loc[:,"model_year" : "origin"])
+
+# items = [
+#     [100,95,70],
+#     [80,75,100],
+#     [90,85,99]
+# ]
+#
+# df_items = pd.DataFrame(items, index=[1,2,3], columns=['a', 'b', 'c'])
+# print(df_items)
+# df_items_melt = pd.melt(df_items).rename(columns={'variable': 'var', 'value': 'val'}).query('val >= 85')
+# print(df_items_melt)
 
