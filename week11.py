7effe2d8455a6c70e3fd846f6d0499d88556eead
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-05-12 18:07:08 +0900

    week11 gui ii

--- week11.py
+++ week11.py
@@ -7,6 +7,8 @@ def click_square():
     input_value = int(en_input.get())
     squared_input_value = square(input_value)
     lbl_display.config(text=f"{input_value}*{input_value} = {squared_input_value}")
+    en_input.delete(0, "end")
+    en_input.focus()
 
 
 def enter_key_square(ev):
@@ -14,12 +16,14 @@ def enter_key_square(ev):
 
 root = tk.Tk()
 root.geometry("400x300")
+root.title("GUI BASIC")
 
-lbl_display = tk.Label(text = "결과 디스플레이 창")
-en_input = tk.Entry()
-btn_calculate = tk.Button(text = "제곱 연산", command = click_square)
+lbl_display = tk.Label(root, text = "결과 디스플레이 창")
+en_input = tk.Entry(root)
+btn_calculate = tk.Button(root,text = "제곱 연산", command = click_square)
 
 en_input.bind("<Return>", enter_key_square)
+en_input.focus()
 
 lbl_display.pack()
 en_input.pack(fill = "x")
