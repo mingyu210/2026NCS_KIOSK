67473487c4768fcb6cdd40b99b929308790bff79
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-05-19 18:16:07 +0900

    week12 web v0.3

--- week12.py
+++ week12.py
@@ -6,16 +6,13 @@ html = """
 <title>스크레이핑 실습</title>
 </head>
 <body>
-<h1 id="univ">인하대학교</h1>
-<p>웹스크레이핑</p>
-<p id="contents">넘파이, 판다스, NLP ... </p>
+<a href="http://www.inha.ac.kr">인하대학교</a><br>
+<a href="http://www.harvard.edu">하버드대학교</a>
 </body>
 </html>
 """
-
 soup = BeautifulSoup(html, 'html.parser')
-university = soup.find(id='univ')
-contents = soup.find(id='contents')
-
-print(contents.string)
-print(university.string)
+urls = soup.find_all("a")
+#print(urls)
+for url in urls:
+  print(f"{url.string}의 url주소는 {url.attrs['href']}입니다.")
