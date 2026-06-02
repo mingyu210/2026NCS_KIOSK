628f914a228efbd87f799bdb722510e36f154a06
DESKTOP-V4TASM6\user <baemingyu5@gmail.com>
2026-06-02 17:10:59 +0900

    week14 thread ii

--- week14.py
+++ week14.py
@@ -1,18 +1,26 @@
-# function based
-# import threading
-#
-# def worker():
-#     print("work")
-#
-# t = threading.Thread(target=worker)
-# t.start()
-
-# class based
 import threading
+import time
 
-class MyThread(threading.Thread):
-    def run(self):
-        print("work")
-t = MyThread()
-t.start()
-t.join()
+def print_numbers():
+    for i in range(1, 6):
+        print(f"Number: {i}")
+        time.sleep(1.5)
+
+def print_letters():
+    for letter in ['A', 'B', 'C', 'D', 'E']:
+        print(f"Letter: {letter}")
+        time.sleep(1)
+
+# print_numbers()
+# print_letters()
+
+thread1 = threading.Thread(target=print_numbers)
+thread2 = threading.Thread(target=print_letters)
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+print("All threads are done!")
